1aceb7f19d7b390dba0ab44e96f2cb9efbdd7f3c
Letum <173109312+amoramicae@users.noreply.github.com>
2025-06-09 03:29:28 +0200

    Create DisableComscoreAdsPatch.kt

---
+++ patches/src/main/kotlin/app/revanced/patches/spotify/ads/misc/DisableComscoreAdsPatch.kt
@@ -0,0 +1,29 @@
+package app.revanced.patches.spotify.ads.misc
+
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Patch
+import app.revanced.patcher.annotation.Target
+import app.revanced.patcher.annotation.hook.Hook
+import app.revanced.patcher.annotation.hook.Hook.HookType
+import app.revanced.patcher.extensions.RVParam
+import app.revanced.patcher.util.Logger
+
+@Patch
+@Name("disable-comscore-ads")
+@Description("Disables advertisements by neutralizing the Comscore analytics SDK.")
+object DisableComscoreAdsPatch {
+
+    @Target(
+        value = "com.comscore.android.p002id.IdHelperAndroid",
+        method = "isAdvertisementIdValid",
+        returnType = Boolean::class,
+        parameterTypes = [String::class]
+    )
+    @Hook(HookType.REPLACE)
+    @JvmStatic
+    fun alwaysInvalid(param: RVParam): Boolean {
+        Logger.i("Comscore ad validation hook triggered: Forcing ad to be invalid.")
+        return false
+    }
+}
